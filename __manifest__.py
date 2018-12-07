1162c0bc5153fa26fcd36c08aed79767d3bda9cc
shaojunyang <yang152389@139.com>
2018-12-07 11:43:32 +0000

    添加security

--- __manifest__.py
+++ __manifest__.py
@@ -15,9 +15,9 @@
         'views/training_subject_views.xml',
     # 'views/training_views.xml',
     ],
-    'demo': [
-       'demo/pscloud_demo.xml',
-    ],
+#     'demo': [
+#        'demo/pscloud_demo.xml',
+#     ],
     'qweb': [],
     'js': [],
     'css': [],
