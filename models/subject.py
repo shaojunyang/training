bfb1e94809563a992600a7e322d71837627bc398
shaojunyang <yang152389@139.com>
2018-12-07 11:28:19 +0000

    day3

--- models/subject.py
+++ models/subject.py
@@ -2,13 +2,11 @@
 
 from odoo import api, fields, models
 
-class TrainingSubject(models.Model):
-    _name = 'pscloud.training.subject'
-    _description = "科目"
-
-    name = fields.Char(string='名称')
-    person_id = fields.Many2one('res.partner', string='负责人')
-    lesson_ids = fields.One2many('pscloud.training.lesson', 'subject_id', string='课程')
-    desc = fields.Text(string='描述')
-
-#  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
+class training_subject(Models.model):
+    
+    _name = 'training.subject'
+    _rec_name = 'name'
+    
+    name = fields.Char(u'名称',
+                      size=64,
+                      default=u'默认科目')
